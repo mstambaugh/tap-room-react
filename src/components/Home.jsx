05a2f915ed1b28f1ed486c70df1c346e7c5e9387
Molly Stambaugh <molly.stambaugh@gmail.com>
2019-11-15 16:11:24 -0800

    add more html content

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,10 +4,10 @@ import React from 'react';
 function Home() {
   return (
     <div>
-      <h1>Dream Dive Bar</h1>
+      <h1>Fantasy Dive Bar</h1>
       <hr />
       <div>
-        <p>We are a family run and recently remodeled Portland bar catering to a small but vocal market of annoying midwestern transplants who dig the politics and forests of the PNW, but miss the chill vibes and indisuputably superior craft beers of Great Lakes region. This place is everything: lighting low and slow, booths with privacy curtains, bar stools are also booths. We offer a small rotating selection of hard-to-find craft beers from the Midwest on tap. Our pool table is well-maintained and creepy old pool guys are kicked out when spotted giving unsolicited advice. Our outdoor patio with ping-pong table is covered and heated year-round. We do not and will never offer darts or pinball because they are terrible games.</p>
+        <p><em>We are a family run and recently remodeled Portland bar catering to a small but vocal market of annoying midwestern transplants who dig the politics and forests of the PNW, but miss the chill vibes and indisuputably superior craft beers of the Great Lakes region. We offer a rotating selection of hard-to-find Midwest craft beers on tap, at Midwest prices. We also offer a full-bar because serving nothing but beer is kinda pretentious. Our vibes are maintained with lots of dead lightbulbs, peeling wallpaper, linoleum flooring, and an analog jukebox with selections like CCR, Bryan Adams, and Prince that will most likely steal your quarters. The pool table is well-maintained and our creepy old pool guys are appropriatly monitered; when spotted giving unsolicited advice they are upcharged on drinks and, if they fail to cease and desist, photographed for the bathroom Wall of Shame. The bathrooms are pretty gross, but we are diligent in removing all genres of trite/corny phrases about loving yourself from the bathroom stall walls. Our outdoor patio is covered, heated, smoker-friendly, and sloped in the  ping-pong table area--1 game limit for bros who played ping-pong competitively in college. We also have a 1/2 operational Buck Hunter that will steal at least 1/2 of your quarters. We do not and will never offer darts or pinball because they are terrible games.</em></p>
       </div>
       <hr />
       <div>
