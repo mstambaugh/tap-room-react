4351fc38f97f48c30057a9fb15e9173dddfb4b8e
Student <example@example.com>
2019-11-25 15:44:33 -0800

    add some materialize styling, start working on employeehome taplist display

--- src/components/EmployeeHome.jsx
+++ src/components/EmployeeHome.jsx
@@ -2,78 +2,91 @@ import React from 'react';
 // import Keg from './Keg';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
+// import TapList from './TapList';
 
 
-const QuoteStyles = {
-  color: '#093b14',
+const QuoteCardStyles = {
+  color: '#8da686',
   border: '10px inset #8da686',
   padding: '30px',
-  margin: '50px',
   fontStyle: 'italic',
-  fontSize: '30px',
-  textAlign: 'justify'
+  fontSize: '30px'
 };
 
 const TomWaitsStyles = {
   color: '#c9ba32',
   textAlign: 'right',
-  paddingRight: '50px',
   fontSize: '30px'
 };
 const link3Styling = {
-  color: '#dead9b',
+  color: '#cc8f78',
   fontSize: '25px',
-  // top: '20%',
   fontWeight: 'bold',
-  // position: 'relative',
-  padding: '3px',
-  marginRight: '50px',
-  border: '5px inset #cc8f78',
-  textDecoration: 'none',
   fontFamily: 'Arial',
-  textAlign: 'justify'
+  textDecoration: 'none',
+
 };
 const TitleStyles = {
-  marginBottom: '20px',
-  color: '#8da686',
   borderBottom: '5px inset #81909c',
   fontSize: '40px'
 };
 const TitleStyles2 = {
   marginBottom: '50px',
-  color: '#8da686',
   borderBottom: '5px inset #81909c',
   fontSize: '40px'
 };
-// const TapListStyles = {
-//   margin: '30px',
-//   paddingLeft: '50px',
-//   paddingRight: '50px',
-//   color: '#8da686',
-//   backgroundColor: '#879484',
-// };
-// 
+
+
+const CardStyles = {
+  color: '#8da686',
+  backgroundColor: '#879484',
+  borderLeft: '15px double #c9ba32',
+  padding: '50px',
+  fontSize: '25px',
+  fontFamily: 'Arial',
+};
+
 
 // add verification? Ask if you are rly an employee? Add edit keg, import taplist to employeehome, add buttons to sell/buy pints. 
 function EmployeeHome(props) {
   return (
     <div>
-      <div>
-        <h3 style={QuoteStyles}>A gentleman is someone who can play the accordion, but doesn't.</h3>
-        <h4 style={TomWaitsStyles}>~~~Tom Waits</h4>
-      </div>
-      <div>
-        <h2>Add a New Keg to the TapList?</h2>
-        <button onClick={props.onHandleEmployeeHome}>Yes</button>
-      </div>
-      <h2 style={TitleStyles}>On Tap</h2>
-      <h2 style={TitleStyles2}>Almost tapped Out!</h2>  
+      <div className='row'>
+        <div className='col l10 m10'>
+          <div className='card' style={QuoteCardStyles}>
+            <h3>A gentleman is someone who can play the accordion, but doesn't.</h3>
+            <h4 style={TomWaitsStyles}>~~~Tom Waits</h4>
+          </div>
+          <div className='card' style={CardStyles}>
+            <h2>Add a New Keg to the TapList?</h2>
+            <button onClick={props.onHandleEmployeeHome}>Yes</button>
+            <h2 style={TitleStyles}>On Tap</h2>
+            <h2 className='card'></h2>
 
-      <Link to='/AddKeg' style={link3Styling}>Add a new Keg</Link> 
+            <h2 style={TitleStyles2}>Almost tapped Out!</h2>  
+            <ul>
+              <li style={link3Styling}><Link to='/NewKeg' style={link3Styling}>AddKeg</Link></li> 
+            </ul>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
 EmployeeHome.propTypes = {
   onHandleEmployeeHome: PropTypes.func
 };
+// TapList.propTypes = {
+//   tapList: PropTypes.array
+// };
+// Keg.propTypes = {
+//   name: PropTypes.string.isRequired,
+//   location: PropTypes.string.isRequired,
+//   brand: PropTypes.string.isRequired,
+//   variety: PropTypes.string.isRequired,
+//   price: PropTypes.string.isRequired,
+//   abv: PropTypes.string.isRequired,
+//   pintsLeft: PropTypes.string.isRequired
+// };
+
 export default EmployeeHome; 
